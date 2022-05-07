eb47bce9878f74bcb2475ec027308336dbc25c7a
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-07 21:43:50 +0600

    all data loaded from database to inventory page

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnn5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dbUser:dbUser@cluster0.mtnn5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnn5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
 
-  console.log('warehouse of smartphone DB connected');
+async function run(){
 
-  // perform actions on the collection object
-  client.close();
-});
+  try{
+    await client.connect();
+    const productCollection = client.db('wareHouse').collection('product');
+    
+    app.get('/product', async(req,res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+    
+    
+
+
+  }
+  finally{
+
+  }
+
+}
+
+run().catch(console.dir);
+
 
 
 
