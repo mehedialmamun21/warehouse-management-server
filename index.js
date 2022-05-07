c3f552570840b01ee73fd12f657bffa74300ef52
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-08 05:42:50 +0600

    DELETE api done for myItems section each product deletion

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ async function run(){
       res.send(result);
     })
 
+    // DELETE
+
+    app.delete('/product/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
   }
   finally{
